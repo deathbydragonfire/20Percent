93c7ef19e26cf38f2dfb71b7bb244e9410503991
Hanna Kessler <Hanna Kessler>
2016-02-28 20:35:47 -0700

    Update with first deliverable

---
+++ GoingPostal/Assets/Scripts/BeltMoves.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BeltMoves : MonoBehaviour {
+    public float speed = 0.1f;
+	// Use this for initialization
+	void Start () {
+	    transform.position = new Vector2(-8.0f,-2.38f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.x < 8.0f) { transform.position = new Vector2(transform.position.x + 1.0f* speed, transform.position.y);}
+        else { transform.position = new Vector2(-8.0f, -2.38f); }
+	}
+}
---
+++ GoingPostal/Assets/Scripts/Touch.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Touch : MonoBehaviour {
+    SpriteRenderer sr;
+    
+   
+	// Use this for initialization
+	void Start () {
+        sr = GetComponent<SpriteRenderer>();
+        sr.color = new Color(255, 255, 255, 255);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.touchCount > 0)
+        {
+            Vector3 pos = Input.GetTouch(0).position;
+            Debug.Log("Touch " + pos);
+
+            Ray ray = Camera.main.ScreenPointToRay(pos);
+            if (Physics.Raycast(ray))
+            {
+                Debug.Log("Something hit");
+                sr.color = new Color(100, 100, 100, 255);
+            }
+
+        }
+	}
+}
